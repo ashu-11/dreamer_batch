68eb9b94014141dcc9ef7c90a5622ff3d0a0f2f6
Ashutosh Mishra <ashutosh.shai84@gmail.com>
2022-12-06 22:32:24 +0530

    Create createContext.js
    created the contect

---
+++ createContext.js
@@ -0,0 +1,10 @@
+// we're creating the context for this applcation 
+// setup the context for the application 
+
+import { createContext} from "react";
+
+const counterConext = createContext();
+export default counterConext;
+
+
+//  prodcut---------->audince{consumer}--------how they will distibute/awareness about their---provider
